078ba07f192f14f7b5361e249f4694c45ce0df64
Oliver Lumby <me@oliverlumby.com>
2023-02-20 17:11:43 +0100

    add some tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,25 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+use function Pest\Laravel\artisan;
+
+it('can test command', function () {
+    artisan('make:pivot', ['table1' => 'test1', 'table2' => 'test2'])->assertSuccessful();
+});
+
+it('can test generate migration', function () {
+    artisan('make:pivot', ['table1' => 'test', 'table2' => 'test'])
+        ->assertSuccessful()
+        ->expectsOutputToContain('created successfully');
+});
+
+it('can test generate migration with correct name', function () {
+    artisan('make:pivot', ['table1' => 'alpha', 'table2' => 'bravo'])
+        ->assertSuccessful()
+        ->expectsOutputToContain('create_alphas_bravos_table');
+});
+
+it('can test alphabetise migration name', function () {
+    artisan('make:pivot', ['table1' => 'bravo', 'table2' => 'alpha'])
+        ->assertSuccessful()
+        ->expectsOutputToContain('create_alphas_bravos_table');
 });
